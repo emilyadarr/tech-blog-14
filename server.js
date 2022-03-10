1e767690dc442e9189be197cf2ac0b1f08ef1a86
Emily <emilyadarr@gmail.com>
2022-03-09 20:27:16 -0500

    fix session secret

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SESS_SECRET,
+  secret: 'super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
